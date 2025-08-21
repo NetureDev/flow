ecac18f705887752a055f2f000107522f494d770
NetureDev <lifecycle702@gmail.com>
2025-08-21 18:53:44 -0400

    Create install.sh

---
+++ fun/1.0/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+read -p "Start Matrix rain? (y/n): " confirm
+
+clear
+if [[ $confirm == "y" || $confirm == "Y" ]]; then
+  echo -e "\033[32my\033[0m"
+  sleep 3
+else
+  echo -e "\033[31mn\033[0m"
+  sleep 3
+  exit 0
+fi
+
+tput civis
+while :; do
+  echo -e "\033[32m$(jot -r 1 20 80 | awk '{for(i=0;i<$1;i++) printf " "; printf "%c", 33+int(rand()*94)}')\033[0m"
+  sleep 0.05
+done
